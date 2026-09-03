1a157520da6fcfba79878537723beb2a75e59542
Juan Ruiz <juaruizra@gmail.com>
2026-09-02 22:46:45 -0500

    feat: add 3x3 nut piece with central hole and update game mechanics

--- game.js
+++ game.js
@@ -13,7 +13,11 @@ const COLORS = [
   '#e57373', // Z - red
   '#7986cb', // J - indigo
   '#ffb74d', // L - orange
+  '#9e9e9e', // Nut - metallic grey
 ];
+
+const NUT = 8;
+const NUT_CHANCE = 0.10;
 
 const PIECES = [
   null,
@@ -24,6 +28,7 @@ const PIECES = [
   [[5,5,0],[0,5,5],[0,0,0]],                  // Z
   [[6,0,0],[6,6,6],[0,0,0]],                  // J
   [[0,0,7],[7,7,7],[0,0,0]],                  // L
+  [[8,8,8],[8,0,8],[8,8,8]],                  // Nut (hueco central)
 ];
 
 const LINE_SCORES = [0, 100, 300, 500, 800];
@@ -55,7 +60,7 @@ function createBoard() {
 }
 
 function randomPiece() {
-  const type = Math.floor(Math.random() * 7) + 1;
+  const type = Math.random() < NUT_CHANCE ? NUT : Math.floor(Math.random() * 7) + 1;
   const shape = PIECES[type].map(row => [...row]);
   return { type, shape, x: Math.floor(COLS / 2) - Math.floor(shape[0].length / 2), y: 0 };
 }
